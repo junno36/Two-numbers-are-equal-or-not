2c4a2586a3020c647a7bb089f5c5bac07d54f004
junno36 <108143721+junno36@users.noreply.github.com>
2022-07-31 13:17:39 +0600

    Update condition1.c

--- condition1.c
+++ condition1.c
@@ -4,7 +4,9 @@ int main()
     int a,b;
     scanf("%d %d",&a, &b);
     if (a==b)
+    {
     printf("Number1 and Number2 are equal.");
+    }
     else
     {
         printf("Number1 and Number2 are not equal.");
